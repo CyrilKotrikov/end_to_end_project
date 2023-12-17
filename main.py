5e1ac7366c5849c0f4b0968a9817fbe16f052ba7
CyrilKotrikov <kotrikov@gmail.com>
2023-12-17 21:22:28 +0200

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import torch
+
+model_loaded = torch.jit.load("/Users/kirillkotrikov/Documents/End to end project/end_to_end_project/wine_model_scripted.pt")
+model_loaded.eval()
+
 
 class Item(BaseModel):
     parameter_1: float
@@ -13,4 +18,5 @@ async def prediction(item: Item):
     
     item_dict = item.dict()
 
-    return item.parameter_1 + item.parameter_2
+    return torch.argmax(model_loaded(torch.tensor([[item.parameter_1,item.parameter_2]], dtype=torch.float32))).item()
+    
